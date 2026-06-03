53a16386d54720d876cc4215c835a8f45a3f6d59
Adeilson Souza <adessouza@gmail.com>
2026-06-03 10:30:48 -0400

    feat(6modelo): treina, avalia e extrai metricas da rede neural

---
+++ 6modelo/modelcreation.py
@@ -0,0 +1,131 @@
+
+#Pipeline de limpeza, tratamento, pre procesamento e criacao do modelo de Machine Learning
+
+#importar bibliotecas
+
+import pandas as pd
+from datetime import datetime
+import numpy as np
+import random as python_random
+import joblib
+
+from sklearn.preprocessing import StandardScaler,LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import RFE
+import tensorflow as tf
+
+from utils import *
+import const
+
+#reproduzindo o mesmo resultado pra fins de comparacao
+seed = 41
+np.random.seed(seed)
+python_random.seed(seed)
+tf.random.set_seed(seed)
+
+#obtendo dados brutos
+df = fetch_data_from_db(const.consulta_sql)
+
+#convertendo tipos
+df['idade'] = df['idade'].astype(int)
+df['valorsolicitado'] = df['valorsolicitado'].astype(float)
+df['valortotalbem'] = df['valortotalbem'].astype(float)
+
+
+#Tratamento de Erros de Digitação especificamente a conluna profissoes em que há erros de digitacao
+profissoes_validas = ['Advogado', 'Arquiteto', 'Cientista de Dados', 'Contador','Dentista','Empresário',
+                                       'Engenheiro','Médico','Programador']
+corrigir_erros_digitacao(df, 'profissao', profissoes_validas)
+
+
+#Tratamento de nulos
+df = substitui_nulos(df)
+
+#Tratamento de Outliers, substitiu min,max pela mediana
+df = tratar_outliers(df, 'tempoprofissao', 0, 70)
+df = tratar_outliers(df, 'idade', 0, 110)
+
+#Feature Engineering: interaction para criar novos atributos para avaliar o impacto na criacao do modelo
+df['proporcaosolicitadototal'] = df['valorsolicitado'] / df['valortotalbem']
+df['proporcaosolicitadototal'] = df['proporcaosolicitadototal'].astype(float)
+
+# Dividindo os Dados em treino e teste, X variaveis independentes e y variavel dependente(classe)
+X = df.drop('classe', axis=1)
+y = df['classe']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, 
+                                                    random_state=seed)# 20% dos dados
+
+# Normalizando variaveis numericas X, boa pratica dividir em treino e teste antes de normalizar
+X_test = save_scalers(X_test, ['tempoprofissao','renda','idade',
+                               'dependentes','valorsolicitado','valortotalbem','proporcaosolicitadototal'])
+X_train = save_scalers(X_train, ['tempoprofissao','renda','idade',
+                                 'dependentes','valorsolicitado','valortotalbem','proporcaosolicitadototal'])
+
+#Codificando dados categoricos em dados numericos, isso é feito de maneiras diferentes para X e y 
+#por questoes de intuitividade das classes ruim=0 e bom=1
+mapeamento = {'ruim': 0, 'bom': 1}
+y_train = np.array([mapeamento[item] for item in y_train])
+y_test = np.array([mapeamento[item] for item in y_test])
+X_train = save_encoders(X_train, ['profissao', 'tiporesidencia', 
+                                  'escolaridade','score','estadocivil','produto'])
+X_test = save_encoders(X_test, ['profissao', 'tiporesidencia', 
+                                'escolaridade','score','estadocivil','produto'])
+
+
+# Selecionando Atributos com Random Forest baseado na importância para otimizar a entrada da Rede Neural.
+model = RandomForestClassifier()
+# Instancia o RFE
+selector = RFE(model, n_features_to_select=10, step=1)
+selector = selector.fit(X_train, y_train)
+# Transforma os dados
+X_train = selector.transform(X_train)
+X_test = selector.transform(X_test)
+joblib.dump(selector, './objects/selector.joblib')
+
+
+#contruindo a rede neural com keras tensorflow
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(128, activation='relu', input_shape=(X_train.shape[1],)), #o numero de neuronios da camada de entrada é o shape[1] dos dados de treino = numero de colunas
+    tf.keras.layers.Dropout(0.3), #minimiza o overfiting, probalilidade 0.3 de uma camada ser excluida de uma forma aleatoria
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.3),
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dropout(0.3),
+    tf.keras.layers.Dense(1, activation='sigmoid') #camada de saída tem apenas 1 neuronio
+])
+
+
+#Configurando o otimizador, define taxa de aprendizagem (learning rate)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+
+# Compilando o modelo
+model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+
+
+#Treinando o modelo 
+model.fit(
+    X_train,
+    y_train,
+    validation_split=0.2,  #Usa 20% dos dados para validação
+    epochs=500,  #numero de vezes que os dados vao passar por toda a rede neural
+    batch_size=10, #cada 10 conjuntos de registros passado, atualiza os pesos
+    verbose=1 #gerar saída do que está produzindo
+)
+model.save('meu_modelo.keras') #salvando o modelo
+
+
+#Previsões
+y_pred = model.predict(X_test)
+y_pred = (y_pred > 0.5).astype(int)  
+
+
+#Avaliando o modelo
+print("Avaliação do Modelo nos Dados de Teste:")
+model.evaluate(X_test, y_test)
+
+
+#Métricas de classificação
+print("\nRelatório de Classificação:")
+print(classification_report(y_test, y_pred))
---
+++ 6modelo/utils.py
@@ -0,0 +1,100 @@
+#funcoes que serao utilizadas durante o pre processamento e criacao do modelo
+
+#importar bibliotecas
+from fuzzywuzzy import process #logica difusa
+import pandas as pd
+from sklearn.preprocessing import StandardScaler,LabelEncoder # normalizar dados e variaveis categoricas
+import joblib # salvar os objetos
+import yaml # ler arquivo de configuracoes
+import psycopg2 # conectar ao banco de dados
+import const #arquivo de constante
+
+
+#recuperar os dados atualizados do BD
+def fetch_data_from_db(sql_query):
+    try:
+        with open('config.yaml', 'r') as file: # arquivo de configuracoes de conexao
+            config = yaml.safe_load(file)
+
+        con = psycopg2.connect(
+            dbname=config['database_config']['dbname'], 
+            user=config['database_config']['user'], 
+            password=config['database_config']['password'], 
+            host=config['database_config']['host']
+        )
+
+        # executar a consulta sql recebida como parametro
+        cursor = con.cursor()
+        cursor.execute(sql_query)
+
+        # retornar todos os registros
+        df = pd.DataFrame(cursor.fetchall(), columns=[desc[0] for desc in cursor.description])
+
+    #liberar os recursos
+    finally:
+        if 'cursor' in locals():
+            cursor.close()
+        if 'con' in locals():
+            con.close()
+
+    #retorna o date frame
+    return df
+
+#corrigir erros de digitacao com logica difusa
+def corrigir_erros_digitacao(df, coluna, lista_valida):
+    for idx, linha in df.iterrows():
+        valor = linha[coluna]
+        
+        if pd.isnull(valor):
+            continue
+            
+        valor_str = str(valor).strip()
+        
+        if valor_str not in lista_valida:
+            resultado = process.extractOne(valor_str, lista_valida)
+            if resultado:
+                # Pega estritamente apenas o texto corrigido da tupla
+                df.at[idx, coluna] = resultado[0]
+
+
+#substituir valores nulos
+def substitui_nulos(df):
+    for coluna in df.columns:
+        # Verifica se a coluna é do tipo numérico
+        if pd.api.types.is_numeric_dtype(df[coluna]):
+            mediana = df[coluna].median()
+            df[coluna] = df[coluna].fillna(mediana)
+        else:
+            # Para colunas de texto/object, calcula a moda se houver valores
+            if not df[coluna].dropna().empty:
+                moda = df[coluna].mode()[0]
+                df[coluna] = df[coluna].fillna(moda)
+                
+    return df
+
+
+#tratar outliers numericos com mediana
+def tratar_outliers(df, coluna, minimo, maximo):
+    mediana = df[(df[coluna] >= minimo) & (df[coluna] <= maximo)][coluna].median()
+    df[coluna] = df[coluna].apply(lambda x: mediana if x < minimo or x > maximo else x)
+    return df
+
+
+#normalizar dados numericos
+def save_scalers(df, nome_colunas):
+    for nome_coluna in nome_colunas:
+        scaler = StandardScaler()
+        df[nome_coluna] = scaler.fit_transform(df[[nome_coluna]])
+        joblib.dump(scaler, f"./objects/scaler{nome_coluna}.joblib")
+
+    return df
+
+
+#decodificar variaveis categoricas
+def save_encoders(df, nome_colunas):
+    for nome_coluna in nome_colunas:
+        label_encoder = LabelEncoder()
+        df[nome_coluna] = label_encoder.fit_transform(df[nome_coluna])
+        joblib.dump(label_encoder, f"./objects/labelencoder{nome_coluna}.joblib")
+
+    return df
